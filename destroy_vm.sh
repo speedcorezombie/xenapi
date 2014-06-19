dcc520caca252543d36a09f55a853d4bc645bc05
speedcorezombie <speedcorezombie@gmail.com>
2014-06-19 20:23:55 +0400

    destroy_vm added

---
+++ destroy_vm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Parameters:
+# name
+if [ $# -lt 1  ]; then
+        echo "Usage: destroy_vm.sh NAME"
+        exit 1
+fi
+
+source ./conf.sh
+NAME=$1
+DISK=${NAME}
+SWAP=${NAME}_swap
+
+virsh destroy ${NAME}
+virsh undefine ${NAME}
+lvremove /dev/${VG}/${DISK}
+lvremove /dev/${VG}/${SWAP}
